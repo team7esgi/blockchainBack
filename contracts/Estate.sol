ebc4ddc69db2897ae103c7e96c069481dcff5f3d
john <allou.john@hotmail.fr>
2020-11-17 19:12:04 +0100

    premier push

---
+++ contracts/Estate.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.4.22 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract Estate {
+
+    string  surface;
+    string category;
+    uint8 nbRoom;
+    uint8 nbBedRoom;
+    Other[] others;
+    string about;
+    string title;
+    string streetName;
+    uint16 price;
+
+    struct Other{
+        string item;
+        string description;
+    }
+
+    constructor( string memory _surface, string memory  _category, uint8  _nbRoom, uint8 _nbBedRoom,
+            Other[] memory  _others, string memory  _about, string memory  _title,
+            string memory _streetName, uint16 _price) public {
+
+        surface = _surface;
+        category = _category;
+        nbRoom = _nbRoom;
+        nbBedRoom = _nbBedRoom;
+        others = _others;
+        about = _about;
+        title = _title;
+        streetName = _streetName;
+        price = _price;
+
+    }
+
+
+
+}
