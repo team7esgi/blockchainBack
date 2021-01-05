aec43dd0d445f0d1e3656fd7ecaff8cea74d4bbd
marcglm <marcghalem@hotmail.fr>
2021-01-05 22:45:09 +0100

    ajout event et update createEstate

--- contracts/EstateFactory.sol
+++ contracts/EstateFactory.sol
@@ -5,6 +5,20 @@ pragma experimental ABIEncoderV2;
 
 contract EstateFactory is Ownable {
 
+    event NewEstate(uint zombieId, string name, uint dna);
+
+    struct Other{
+        string item;
+        string description;
+    }
+
+    struct Street{
+        string rue;
+        string nom;
+        string codePostale;
+        string ville;
+        string pays;
+    }
 
     struct Estate{
         uint8  surface;
@@ -20,22 +34,28 @@ contract EstateFactory is Ownable {
 
     }
 
-    struct Other{
-        string item;
-        string description;
+    Estate[] public estates;
+
+    mapping (uint => address) public estateToOwner;
+    mapping (address => uint) ownerEstateCount;
+
+    function createOther(string _item, string _description) private{
+        return Other(_item,_description);
     }
 
-    struct Street{
-        string rue;
-        string nom;
-        string codePostale;
-        string ville;
-        string pays;
-    }
-
-    function createEstate( uint8  _surface, string memory  _category, uint8  _nbRoom, uint8 _nbBedRoom,
-            string memory  _about, string memory  _title,
-            Street memory _street, uint16 _price) internal {
+    function createEstate(
+        uint8 _surface,
+        string memory _category,
+        uint8 _nbRoom, 
+        uint8 _nbBedRoom,
+        string memory _about,
+        string memory _title,
+        uint16 _price,
+        string _rue, 
+        string _nom, 
+        string _codePostale, 
+        string _ville, 
+        string _pays) private {
 
         Estate memory newEstate;
 
@@ -43,10 +63,16 @@ contract EstateFactory is Ownable {
         newEstate.category = _category;
         newEstate.nbRoom = _nbRoom;
         newEstate.nbBedRoom = _nbBedRoom;
+        newEstate.others =
         newEstate.about = _about;
         newEstate.title = _title;
-        newEstate.street = _street;
+        newEstate.street = Street(_rue, _nom, _codePostale, _ville, _pays);
         newEstate.price = _price;
+
+        uint idEstate = estates.push(newEstate) - 1;
+        estateToOwner[id] = msg.sender;
+        ownerEstateCount[msg.sender]++;
+
 
     }
 
